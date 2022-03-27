c382362273a4b16d0d84dab5820552478f022d15
David Sommer <david.sommer@myposter.de>
2022-03-27 03:12:37 +0200

    Debug

--- src/delete-untagged.js
+++ src/delete-untagged.js
@@ -1,15 +1,15 @@
-module.exports = async ({github, context}) => {
-    const shouldDeleteUntagged = (0 + context.inputs.delete_untagged) > 0;
+module.exports = async ({github, inputs}) => {
+    const shouldDeleteUntagged = (0 + inputs.delete_untagged) > 0;
 
     if (!shouldDeleteUntagged) {
         console.log("Delete of untagged images is disabled. Set 'delete_untagged':1 to enable it");
     } else {
-        const keepVersions = 0 + context.inputs.keep_versions_untagged;
+        const keepVersions = 0 + inputs.keep_versions_untagged;
 
-        console.log("Checking package " + context.inputs.owner + "/" + context.inputs.package + " for untagged versions");
+        console.log("Checking package " + inputs.owner + "/" + inputs.package + " for untagged versions");
         console.log("Keeping (at least) " + keepVersions + " untagged versions");
 
-        const response = await github.request("GET /" + context.inputs.owner + "/packages/container/" + context.inputs.package + "/versions", {
+        const response = await github.request("GET /" + inputs.owner + "/packages/container/" + inputs.package + "/versions", {
             per_page: 100
         });
 
@@ -24,7 +24,7 @@ module.exports = async ({github, context}) => {
             }
 
             console.log("delete " + version.id + " (Created at: " + version.created_at + ")");
-            const deleteResponse = await github.request("DELETE /" + context.inputs.owner + "/packages/container/" + context.inputs.package + "/versions/" + version.id, {});
+            const deleteResponse = await github.request("DELETE /" + inputs.owner + "/packages/container/" + inputs.package + "/versions/" + version.id, {});
             if (deleteResponse.status >= 200 && deleteResponse.status < 300) {
                 console.log("Successfully deleted");
                 deleteCount++;
--- src/delete.js
+++ src/delete.js
@@ -1,13 +1,13 @@
-module.exports = async ({github, context}) => {
-    console.log("Deleting old versions from " + context.inputs.owner + "/" + context.inputs.package);
-    console.log("Keeping (at least) " + context.inputs.keep_versions + " versions");
-    console.log("Will skip images that are older than ${{ context.inputs.minimum_days }} days");
-    console.log("Skipping images with tags ${{ context.inputs.skip_tags }}");
+module.exports = async ({github, inputs}) => {
+    console.log("Deleting old versions from " + inputs.owner + "/" + inputs.package);
+    console.log("Keeping (at least) " + inputs.keep_versions + " versions");
+    console.log("Will skip images that are older than ${{ inputs.minimum_days }} days");
+    console.log("Skipping images with tags ${{ inputs.skip_tags }}");
 
-    const keepVersions = 0 + context.inputs.keep_versions;
-    const skipTags     = context.inputs.skip_tags.split(',');
-    const minimumAge   = new Date(new Date().setDate(new Date().getDate() - context.inputs.minimum_days));
-    const response     = await github.request("GET /" + context.inputs.owner + "/packages/container/" + context.inputs.package + "/versions", {
+    const keepVersions = 0 + inputs.keep_versions;
+    const skipTags     = inputs.skip_tags.split(',');
+    const minimumAge   = new Date(new Date().setDate(new Date().getDate() - inputs.minimum_days));
+    const response     = await github.request("GET /" + inputs.owner + "/packages/container/" + inputs.package + "/versions", {
         per_page: 100,
         page:     1
     });
@@ -40,7 +40,7 @@ module.exports = async ({github, context}) => {
         }
 
         console.log("Deleting " + message);
-        const deleteResponse = await github.request("DELETE /" + context.inputs.owner + "/packages/container/" + context.inputs.package + "/versions/" + version.id, {});
+        const deleteResponse = await github.request("DELETE /" + inputs.owner + "/packages/container/" + inputs.package + "/versions/" + version.id, {});
         if (deleteResponse.status >= 200 && deleteResponse.status < 300) {
             console.log("Successfully deleted");
             deleteCount++;
